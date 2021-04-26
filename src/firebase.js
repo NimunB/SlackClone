e4e5f26efecf6924aa9798bccf86c816686863d3
Nimun Bajwa <nimun2001@gmail.com>
2021-04-26 13:14:08 -0400

    initial commit with top bar

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCh7ySwqZ6SJeHDgd0fHz9KMDr4UOMvCZU",
+    authDomain: "slack-clone-yt-28c9a.firebaseapp.com",
+    projectId: "slack-clone-yt-28c9a",
+    storageBucket: "slack-clone-yt-28c9a.appspot.com",
+    messagingSenderId: "10850686313",
+    appId: "1:10850686313:web:12ca9c3b1a0d27dbdfd9f8"
+  };
