ac2e0efc911a801779c5cf98f6ef5f196ae87f80
Nimun Bajwa <nimun2001@gmail.com>
2021-04-27 22:53:07 -0400

    Search bar in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,19 +2,24 @@ import React from 'react'
 import styled from "styled-components";
 import {Avatar} from "@material-ui/core";
 import AccessTimeIcon from '@material-ui/icons/AccessTime';
+import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return <HeaderContainer>
         {/* Header left */}
         <HeaderLeft>
             <HeaderAvatar>
-                {/* TODO Add onclick*/}
-                
-
+                {/* TODO Add onclick */}
             </HeaderAvatar>
             <AccessTimeIcon />
         </HeaderLeft>
+
         {/* Header search */}
+        <HeaderSearch>
+            <SearchIcon />
+            <input placeholder = "Search PAPAFAM" />
+        </HeaderSearch>
+
         {/* Header right */}
     </HeaderContainer>
 }
@@ -22,6 +27,30 @@ function Header() {
 export default Header;
 
 // Styled components
+const HeaderSearch = styled.div`
+    flex: 0.4;
+    opacity: 1;
+    border-radius: 6px;
+    background-color: #421f44;
+    text-align: center;
+    display: flex;
+    padding: 0 50px;
+    color: gray;
+    border: 1px gray solid;
+
+    > input {
+        background-color: transparent;
+        border: none;
+        text-align: center;
+        min-width: 30 vw;
+        outline: 0; // no outline
+        color: white;
+
+
+
+    }
+
+`
 const HeaderContainer = styled.div`
     display: flex;
     position: fixed;
@@ -40,6 +69,7 @@ const HeaderLeft = styled.div`
     align-items: center;
     margin-left: 20px;
 
+    /* icon inside goes inside */
     > .MuiSvgIcon-root {
         margin-left: auto;
         margin-right: 30px;
